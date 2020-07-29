3ed6292e85bb554f551affa9b86b3978b8a604d5
Maksim Levin <levmaxmih88@mail.ru>
2020-07-30 01:28:30 +1000

    HW07 Implemented MealRestController plus tests

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,17 +1,35 @@
 package ru.javawebinar.topjava.web;
 
 import org.assertj.core.matcher.AssertionMatcher;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.web.SecurityUtil.*;
 
 class RootControllerTest extends AbstractControllerTest {
+    private static int userId;
+
+    @BeforeAll
+    static void beforeAll() {
+        userId = authUserId();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        setAuthUserId(userId);
+    }
 
     @Test
     void getUsers() throws Exception {
@@ -28,5 +46,32 @@ class RootControllerTest extends AbstractControllerTest {
                             }
                         }
                 ));
+    }
+
+    private void getMealHelper(List<Meal> meals) throws Exception {
+        perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals",
+                        new AssertionMatcher<List<MealTo>>() {
+                            @Override
+                            public void assertion(List<MealTo> actual) throws AssertionError {
+                                MEAL_TO_MATCHER.assertMatch(actual, MealsUtil.getTos(meals, authUserCaloriesPerDay()));
+                            }
+                        }
+                ));
+    }
+
+    @Test
+    void getMealsForUser() throws Exception {
+        setAuthUserId(USER_ID);
+        getMealHelper(MEALS);
+    }
+
+    @Test
+    void getMealsForAdmin() throws Exception {
+        setAuthUserId(ADMIN_ID);
+        getMealHelper(ADMIN_MEALS);
     }
 }
