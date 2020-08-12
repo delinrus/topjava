6b0a0bc173082f50d3989942607efee9e19ae2bd
Maksim Levin <levmaxmih88@mail.ru>
2020-08-13 05:11:11 +1000

    HW09 Optional added datetimepicker

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -63,4 +63,27 @@ $(function () {
         }),
         updateTable: updateFilteredTable
     });
+
+    $('#startDate').datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+    });
+
+    $('#endDate').datetimepicker({
+        timepicker: false,
+        format: 'Y-m-d',
+        formatDate: 'Y-m-d',
+    });
+
+    $('#startTime').datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+    });
+
+    $('#endTime').datetimepicker({
+        datepicker: false,
+        format: 'H:i',
+    });
+
 });
